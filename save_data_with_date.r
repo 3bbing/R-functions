59c1d825a201692d1b94733ad0296f80b59daaee
3bbing <2be.ebbing@gmail.com>
2019-09-04 05:46:30 +0200

    added: pass object directly + Windows compatibility

--- save_data_with_date.r
+++ save_data_with_date.r
@@ -5,7 +5,7 @@ saveMe <- function(df){
   dfname <- deparse(substitute(df))
   # strip all whitespaces from the name, add the date and time, add extension. 
   # Save in folder "data" so as to separate data from scripts (remember to create if you have not got it yet.)
-  filename <- gsub(" ", "", paste("data/", dfname, "_", Sys.time(), ".Rda"))
+  filename <- gsub(" ", "", paste("data/", dfname, "_", gsub(":", "_", gsub(" ", "_", Sys.time())), ".Rda"))
   save(df, file = filename)
   # remove the variable
   rm(filename)
